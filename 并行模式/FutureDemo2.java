e90437173b3d715e6fccf6e849091450d2be3bf4
Kevin-chen-sheng <52196486+Kevin-chen-sheng@users.noreply.github.com>
2020-06-14 09:08:48 +0800

    通过Future实现,RealData需要实现Callable接口.
    与上述FutureTask不同的是, RealData需要实现Callable接口.

---
+++ 并行模式/FutureDemo2.java
@@ -0,0 +1,36 @@
+public class FutureDemo2 {
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        ExecutorService service = Executors.newCachedThreadPool();
+        Future<String> future = service.submit(new RealData2());
+
+        System.out.println("RealData2方法调用完毕");
+        // 模拟主函数中其他耗时操作
+        doOtherThing();
+        // 获取RealData2方法的结果
+        System.out.println(future.get());
+    }
+
+    private static void doOtherThing() throws InterruptedException {
+        Thread.sleep(2000L);
+    }
+}
+
+class RealData2 implements Callable<String>{
+
+    public String costTime() {
+        try {
+            // 模拟RealData耗时操作
+            Thread.sleep(1000L);
+            return "result";
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "exception";
+    }
+
+    @Override
+    public String call() throws Exception {
+        return costTime();
+    }
+}
